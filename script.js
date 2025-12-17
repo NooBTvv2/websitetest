3ab583b94ee44648869ff0851916d82c1976e190
NooBTvv2 <130272845+NooBTvv2@users.noreply.github.com>
2025-12-17 15:02:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
 const text = document.querySelector(".changeText");
 const getHub = document.querySelector(".hub");
-
-setInterval(() => {
-    var w = window.innerWidth
-    var h = window.innerHeight
-
-    getHub.innerHTML = w + ' , ' + h 
-}, 4000)
