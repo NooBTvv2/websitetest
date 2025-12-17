cb02add1ebd169eeaeb50fcb826b4e635b0a839d
NooBTvv2 <130272845+NooBTvv2@users.noreply.github.com>
2025-12-17 12:33:58 +0800

    Display window dimensions every 4 seconds
    Add a setInterval function to display window dimensions.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const text = document.querySelector(".changeText");
+const getHub = document.querySelector(".hub");
+
+setInterval(() => {
+    var w = window.innerWidth
+    var h = window.innerHeight
+
+    getHub.innerHTML = w + ' , ' + h 
+}, 4000)
